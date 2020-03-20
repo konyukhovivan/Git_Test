3c7a878b26e434f51e15f3fab75e6c4d87741c37
Иван Конюхов <KonyukhovIvan@yandex.ru>
2020-03-20 23:49:17 +0300

    Text UI is added.

--- src/ru/Konyukhov/Git/Main.java
+++ src/ru/Konyukhov/Git/Main.java
@@ -8,10 +8,14 @@ public class Main {
 
         Scanner in = new Scanner(System.in);
 
+        System.out.println("Введите количество товара:");
 	    int count = in.nextInt();
+        System.out.println("Введите цену:");
         int price = in.nextInt();
 
         int total = count*price;
+        
+        System.out.println("Стоимость товара:");
         System.out.println(total);
     }
 }
