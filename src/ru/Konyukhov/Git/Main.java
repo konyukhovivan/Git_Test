4248b814c6a8479dbb010a77f2769286f52551b9
Иван Конюхов <KonyukhovIvan@yandex.ru>
2020-03-20 23:34:50 +0300

    Base functional is added.

--- src/ru/Konyukhov/Git/Main.java
+++ src/ru/Konyukhov/Git/Main.java
@@ -3,6 +3,10 @@ package ru.Konyukhov.Git;
 public class Main {
 
     public static void main(String[] args) {
-	    
+	    int count = 2;
+        int price = 29;
+
+        int total = count*price;
+        System.out.println(total);
     }
 }
